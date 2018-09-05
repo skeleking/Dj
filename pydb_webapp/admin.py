95463f3c1dbb3e5b95ea14b0dcb18a5acad8593e
skeleking <5669094@qq.com>
2018-09-05 12:09:11 +0800

    add admin.py

--- pydb_webapp/admin.py
+++ pydb_webapp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+#from pydb_webapp.models import Archs
+
+from pydb_webapp import models
+
+class ArchsAdmin(admin.ModelAdmin):
+    list_display = ('arch_name','root_dir')
+class DirectoriesAdmin(admin.ModelAdmin):
+    list_display = ('dir_name','arch','root_dir')
+
+# class Imgfiles(models.Model):
+#     file_name = models.ImageField(upload_to='img', default= 'none.img')
+#     arch = models.CharField(max_length= my_constants.MAX_NAME_LENGTH, default='none')
+#     outer = models.CharField(max_length= my_constants.MAX_SIMPLE_LENGTH , default='none')
+#     site = models.CharField(max_length= my_constants.MAX_SIMPLE_LENGTH, default='none')
+#     location = models.CharField(max_length= my_constants.MAX_SIMPLE_LENGTH, default='none')
+#     content = models.CharField(max_length= my_constants.MAX_SIMPLE_LENGTH, default='none')
+#     nmch_type= models.CharField(max_length= my_constants.MAX_SIMPLE_LENGTH, default='none')
+#     description = models.CharField(max_length=my_constants.MAX_DESCRIPTION_LENGTH, default='none')
+#     drawing_num = models.CharField(max_length=my_constants.MAX_SIMPLE_LENGTH, default='none')
+class ImagefilesAdmin(admin.ModelAdmin):
+    list_display = ('file_name','arch','outer','site','location','content','nmch_type','description','drawing_num')
+
+#admin.AdminSite.register([models.Archs,models.Directories,models.Imgfiles]    )
+admin.AdminSite.register(admin.site , model_or_iterable=models.Archs,admin_class= ArchsAdmin)
+admin.AdminSite.register(admin.site, model_or_iterable=models.Directories, admin_class= DirectoriesAdmin)
+admin.AdminSite.register(admin.site, model_or_iterable=models.Imgfiles, admin_class= ImagefilesAdmin)
+
